e69f4b7ed56e43e61bd0beab65c85946a978061b
ripps818 <ripps818@gmail.com>
2026-01-23 19:01:46 -0600

    add scx-manager, lsfg-vk-ui, antimicrox packages enable scx_loader.service Add custom game-performance script Add custom tuned recommend Don't generate files in build.sh, instead copy them from system_files

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,13 +3,6 @@
 set -ouex pipefail
 
 ### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# 1. Repositories
 # --- CachyOS ---
 dnf5 -y copr enable bieszczaders/kernel-cachyos
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
@@ -24,7 +17,6 @@ dnf5 -y copr enable che/nerd-fonts
 # --- Mullvad ---
 curl -fsSL https://repository.mullvad.net/rpm/stable/mullvad.repo -o /etc/yum.repos.d/mullvad.repo
 
-# 2. Removals
 # Removing stock kernel and drivers to replace with CachyOS
 dnf5 remove -y \
     kernel \
@@ -34,7 +26,7 @@ dnf5 remove -y \
     kernel-modules-extra \
     zram-generator-defaults
 
-# 3. Installation
+# Installation
 
 # Workaround for kernel installation in container:
 # Mask kernel install hooks to prevent dracut failure during dnf transaction
@@ -52,13 +44,16 @@ dnf5 install -y --allowerasing --skip-unavailable \
     cachyos-ksm-settings \
     scx-scheds \
     scx-tools \
+    scx-manager \
     mpv \
     yt-dlp \
     lsfg-vk \
+    lsfg-vk-ui \
     goverlay \
     gpu-screen-recorder-ui \
     gpu-screen-recorder-gtk \
     openrgb \
+    antimicrox \
     coolercontrol \
     lact \
     mullvad-vpn \
@@ -73,6 +68,14 @@ dnf5 install -y --allowerasing --skip-unavailable \
     tealdeer \
     atuin \
     byobu
+
+# Install custom scripts and config files
+install -Dm755 /ctx/system_files/bin/game-performance /usr/bin/game-performance
+install -Dm644 /ctx/system_files/lib/tuned/recommend.conf /usr/lib/tuned/recommend.d/70-bopp-os.conf
+install -Dm644 /ctx/system_files/lib/modprobe.d/amdgpu-overclock.conf /usr/lib/modprobe.d/amdgpu-overclock.conf
+install -Dm644 /ctx/system_files/lib/modprobe.d/kvm-nested.conf /usr/lib/modprobe.d/kvm-nested.conf
+install -Dm644 /ctx/system_files/lib/systemd/ksmd.service /usr/lib/systemd/system/ksmd.service
+install -Dm644 /ctx/system_files/etc/mpv.conf /etc/mpv/mpv.conf
 
 # Unmask hooks
 rm -f /etc/kernel/install.d/05-rpmostree.install
@@ -90,46 +93,9 @@ if [[ -n "$KERNEL_VERSION" ]]; then
     chmod 0600 "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
 fi
 
-## Enable KSMD
-tee "/usr/lib/systemd/system/ksmd.service" > /dev/null <<EOF
-[Unit]
-Description=Activates Kernel Samepage Merging
-ConditionPathExists=/sys/kernel/mm/ksm
-
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/usr/bin/ksmctl -e
-ExecStop=/usr/bin/ksmctl -d
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# 4. Services
-systemctl enable ksmd.service libvirtd.service
+# Services
+systemctl enable ksmd.service libvirtd.service scx_loader.service
 systemctl disable lactd.service coolercontrold.service mullvad-daemon.service tailscaled.service
-
-# 5. Config Files
-# AMD GPU Overclocking Unlock
-mkdir -p /usr/lib/modprobe.d
-echo "options amdgpu ppfeaturemask=0xffffffff" > /usr/lib/modprobe.d/amdgpu-overclock.conf
-
-# AMD Nested Virtualization Unlock
-echo "options kvm_amd nested=1" > /usr/lib/modprobe.d/kvm-nested.conf
-
-# Optimized MPV Config
-# Note: mpv does not support reading config from /usr/lib or /usr/share,
-# so we must place this in /etc/mpv.
-mkdir -p /etc/mpv
-cat <<EOF > /etc/mpv/mpv.conf
-vo=gpu-next
-gpu-api=vulkan
-hwdec=auto-safe
-profile=gpu-hq
-scale=ewa_lanczossharp
-cscale=ewa_lanczossharp
-EOF
 
 # Cleanup /var/tmp used by kernel-install workaround
 rm -rf /var/tmp/*
